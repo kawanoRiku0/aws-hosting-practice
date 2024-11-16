5bdbeda342b4e3240232a97a454b172dd7ce107a
kawanoRiku0 <riku.kawano20020518@gmail.com>
2024-11-16 15:47:50 +0900

    update: RSCでfetchする場合、簡単にデプロイすることが難しいので、一旦CSRで実装

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 // Article構造体の定義
@@ -13,8 +18,25 @@ type Article struct {
 }
 
 func main() {
-	router := gin.Default()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
+	router := gin.Default()
+	// CORSの設定
+	router.Use(cors.New(cors.Config{
+		// 許可するオリジンを指定
+		AllowOrigins: []string{os.Getenv("CLIENT_URL")}, // フロントエンドのURLに合わせて変更
+		// 許可するHTTPメソッドを指定
+		AllowMethods: []string{"GET"},
+		// 許可するHTTPリクエストヘッダーを指定
+		AllowHeaders: []string{"Origin", "Content-Type", "Accept"},
+		// 資格情報の共有を許可するかどうか
+		AllowCredentials: true,
+		// プリフライトリクエストの結果をキャッシュする時間
+		MaxAge: 12 * time.Hour,
+	}))
 	// "/article"エンドポイントへのGETリクエストに対するハンドラー
 	router.GET("/articles", func(c *gin.Context) {
 		articles := []*Article{{
--- frontend/src/app/articles/page.tsx
+++ frontend/src/app/articles/page.tsx
@@ -1,17 +1,54 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
 type Article = {
   title: string;
   content: string;
 };
 
-export default async function Page() {
-  const articles = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/articles`
-  ).then((res) => res.json());
+export default function Page() {
+  const [articles, setArticles] = useState<Article[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/articles`
+        );
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status} ${response.statusText}`);
+        }
+        const data = await response.json();
+        setArticles(data);
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err.message);
+        } else {
+          setError("An unknown error occurred");
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchArticles();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
   return (
     <div>
       <h1>Kawariku AWS Hosting Practice Blog</h1>
-      {articles.map((article: Article) => (
+      {articles.map((article) => (
         <article
           key={article.title}
           className="border border-gray-300 rounded-md p-4 mt-4"
